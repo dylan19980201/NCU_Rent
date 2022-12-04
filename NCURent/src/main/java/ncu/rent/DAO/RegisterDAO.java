e6c15d14262a838cb04442b9d4df23d5e8017ad5
lee <twinkledog@yahoo.com.tw>
2022-12-04 21:07:30 +0800

    classpath

--- NCURent/src/main/java/ncu/rent/DAO/RegisterDAO.java
+++ NCURent/src/main/java/ncu/rent/DAO/RegisterDAO.java
@@ -8,7 +8,7 @@ public class RegisterDAO {
 		String command = "";
 		int type = 1;
 		String []data1 = new String[]{"111421004","421001","Alice","2000-01-01","女","企管碩一","912345678","111421001@cc.ncu.edu.tw"};
-		String []data2 = new String[]{"L0003","khsu0101","許智誠","1972-01-01","男","0912166538","khsu@mgt.ncu.edu.tw"};
+		String []data2 = new String[]{"L0004","khsu0101","許智誠","1972-01-01","男","0912166538","khsu@mgt.ncu.edu.tw"};
 		if(type==0)
 			command = """
 				insert into student (SID,SPassword,SName,SBirth,SGender,SDepartment,SPhone,SEmail)
