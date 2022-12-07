e5b39ff5d06e4a29ce133fc7ce198426a97ad1d8
lee <twinkledog@yahoo.com.tw>
2022-12-07 20:31:39 +0800

    register

--- NCURent/WebContent/register.jsp
+++ NCURent/WebContent/register.jsp
@@ -11,6 +11,20 @@
 	<title>Insert title here</title>
 </head>
 <body>
-	<p>Hello!</p>
+	
+	<form action ="Register" method="post">
+		<div name = "type" value = "1">
+			學生註冊 <%if(request.getAttribute("error")!=null) {%> <%= (request.getAttribute("error")) %> <% } %>
+		</div>
+		帳號:<input type="text" name="id" value = "test1">
+		密碼:<input type="text" name="password" value = "test">
+		姓名:<input type="text" name="name" value = "test">
+		生日:<input type="text" name="birth" value = "test">
+		性別:<input type="text" name="gender" value = "test">
+		系所:<input type="text" name="department" value = "test">
+		手機:<input type="text" name="phone" value = "test">
+		信箱:<input type="text" name="email" value = "test">
+		<input type="submit" value="註冊" class="btn btn-outline-primary">
+	</form>
 </body>
 </html>
--- NCURent/src/main/java/ncu/rent/DAO/RegisterDAO.java
+++ NCURent/src/main/java/ncu/rent/DAO/RegisterDAO.java
@@ -3,13 +3,10 @@ package ncu.rent.DAO;
 import ncu.rent.Database.DBHelper;
 
 public class RegisterDAO {
-    public static void main(String[] args) {
+    public boolean addUserData(String[] user,String type) {
 		DBHelper db = new DBHelper();
 		String command = "";
-		int type = 1;
-		String []data1 = new String[]{"111421004","421001","Alice","2000-01-01","女","企管碩一","912345678","111421001@cc.ncu.edu.tw"};
-		String []data2 = new String[]{"L0004","khsu0101","許智誠","1972-01-01","男","0912166538","khsu@mgt.ncu.edu.tw"};
-		if(type==0)
+		if(type=="1")
 			command = """
 				insert into student (SID,SPassword,SName,SBirth,SGender,SDepartment,SPhone,SEmail)
 				VALUES (?,?,?,?,?,?,?,?)""";
@@ -17,9 +14,9 @@ public class RegisterDAO {
 			command = """
 				insert into landlord (LID,LPassword,LName,LBirth,LGender,LPhone,LEmail)
 				VALUES (?,?,?,?,?,?,?)""";
-		if(db.addUserData(command, data2)==1) 
-		    System.out.println("新增成功");
+		if(db.addUserData(command, user)==1) 
+		    return true;
 		else 
-		    System.out.println("新增失敗");
+		    return false;
 	}
 }
