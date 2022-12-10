9f9bbff5e3a0516cf2606db040d301a658fc1906
dylan kuo <dylan19980201@yahoo.com.tw>
2022-12-10 11:48:52 +0800

    test for upload

--- NCURent/src/main/java/ncu/rent/Controller/PostController.java
+++ NCURent/src/main/java/ncu/rent/Controller/PostController.java
@@ -10,6 +10,9 @@ import ncu.rent.BO.RegisterBO;
 import ncu.rent.DTO.User;
 import org.json.JSONObject;
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import jakarta.servlet.http.Part;
 
@@ -31,27 +34,26 @@ public class PostController extends SuperController{
 	public void Add(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 		Part filePart = request.getPart("file");
 		String fileName = filePart.getSubmittedFileName();
-		
 		for(Part part : request.getParts()) {
-			part.write(path()+"\\HouseImage\\"+fileName);
+			part.write(path()+fileName);
 		}
 		response.getWriter().print("success");
 	}
 	public String path(){
 		String path = getClass().getResource("/").getPath();
-		path = path.substring(1, path.indexOf(".metadata"));
-		path = path.replace("/", "\\");
-		//System.out.println(path);
-		return path;
-		/*
+		path = path.substring(1, path.indexOf(".metadata"))+"\\upload\\houseImage\\";
+		Path p = Paths.get(path);
 		try {
-			File directory = new File("");
-			System.out.println(directory.getCanonicalPath());
-			System.out.println(directory.getAbsolutePath());
+			if(!Files.exists(p)) {
+				Files.createDirectories(p);
+			}
 		}
 		catch(Exception e){
 			System.out.println(e);
 		}
-		*/
+		return path;
+	}
+	public static void main(String args[]) {
+		
 	}
 }
