b89b318bfcac37b9b4e733b1881eebeeb3728022
lee <twinkledog@yahoo.com.tw>
2022-12-19 00:55:45 +0800

    test

--- NCURent/ncu/rent/DAO/HouseDAO.java
+++ NCURent/ncu/rent/DAO/HouseDAO.java
@@ -10,9 +10,12 @@ import ncu.rent.Database.DBHelper;
 
 public class HouseDAO {
 	public List<House> getHouseData(int Hid) {
+	// String command = """
+	// SELECT HAddress as hAddress, HYear as hYear, Rent as rent, Size as size, Equipment as equipment, GenderSpecific as genderSpecific, AID as aID, LName ,HID as hID, PictureName as pictureName,  PostDatetime as postDatetime  
+	// FROM house left join landlord on house.LID = landlord.LID  WHERE aID IS NOT NULL AND  Hid = ?""";
 	String command = """
 	SELECT HAddress as hAddress, HYear as hYear, Rent as rent, Size as size, Equipment as equipment, GenderSpecific as genderSpecific, AID as aID, LName ,HID as hID, PictureName as pictureName,  PostDatetime as postDatetime  
-	FROM house left join landlord on house.LID = landlord.LID  WHERE aID IS NOT NULL AND  Hid = ?""";
+	FROM house left join landlord on house.LID = landlord.LID  WHERE Hid = ?""";
 	List<House> house = new ArrayList<House>();
 	JSONObject condition = new JSONObject();
 	condition.put("Hid", Hid);
